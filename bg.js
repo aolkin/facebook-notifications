3b9af0eb387ec26bf46c2ac29dade35080a521ea
Aaron Olkin <aaron@olkin.net>
2014-01-18 00:54:52 -0500

    Removed extra console.log and added code to show the window on click

--- bg.js
+++ bg.js
@@ -1,12 +1,11 @@
 function processData(data,sender,sendResponse){
-    console.log(data.who,data.msg);
     var notif = webkitNotifications.createNotification('icon.png',data.who,data.msg);
     notif.onclick = function(e) {
 	chrome.tabs.query(
 	    {url: "https://www.facebook.com/*"},
             function(tabs) {
-                chrome.tabs.update(tabs[0].id,
-                                   {selected:true});
+                chrome.tabs.update(tabs[0].id,{selected:true});
+		chrome.windows.update(tabs[0].windowId,{focused:true});
             });
 	this.close();
     };
